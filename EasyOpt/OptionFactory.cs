f51252928bc30a90799e2dfdda03f3efd25f6abe
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2010-05-02 14:01:38 +0000

    Methods to create options were moved to this class.

---
+++ EasyOpt/OptionFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyOpt
+{
+    /**
+     * Class that implements the pattern factory method. Used to retrieve instances of the class Option.
+     */
+
+    class OptionFactory
+    {
+        /**
+         * Creates an option with no parameters.
+         */
+        public static Option<bool> create(bool isRequired, String usageText)
+        {
+            return new SimpleOption(isRequired, usageText);
+        }
+
+        /**
+         * Creates an option with parameter T.
+         */
+        public static Option<T> create<T>(bool isRequired, String usageText, Parameter<T> parameter)
+        {
+            return new ParameterOption<T>(isRequired, usageText, parameter);
+        }
+    }
+}

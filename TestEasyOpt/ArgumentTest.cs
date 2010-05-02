6488758c98246d6b0b9ecc2ad82d17023c4a2ce9
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2010-05-02 14:03:23 +0000

    Commit to visual studio configuration files and added support for unit testing.

---
+++ TestEasyOpt/ArgumentTest.cs
@@ -0,0 +1,128 @@
+﻿using EasyOpt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TestEasyOpt
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ArgumentTest and is intended
+    ///to contain all ArgumentTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ArgumentTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Create
+        ///</summary>
+        [TestMethod()]
+        public void CreateTestDivision()
+        {
+            Argument actualArgument = Argument.Create("--");
+            Assert.AreEqual(ArgumentType.division, actualArgument.Type);
+            Assert.IsNull(actualArgument.Parameter);
+            Assert.IsNull(actualArgument.Name);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+
+        [TestMethod()]
+        public void CreateTestShortOption()
+        {
+            Argument actualArgument = Argument.Create("-v");
+            Assert.AreEqual(ArgumentType.shortOption, actualArgument.Type);
+            Assert.AreEqual("v", actualArgument.Name);
+            Assert.IsNull(actualArgument.Parameter);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+
+        [TestMethod()]
+        public void CreateTestShortOptionWithArgument()
+        {
+            Argument actualArgument = Argument.Create("-vparameter");
+            Assert.AreEqual(ArgumentType.shortOption, actualArgument.Type);
+            Assert.AreEqual("v", actualArgument.Name);
+            Assert.AreEqual("parameter", actualArgument.Parameter);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+        [TestMethod()]
+        public void CreateTestLongOption()
+        {
+            Argument actualArgument = Argument.Create("--v");
+            Assert.AreEqual(ArgumentType.longOption, actualArgument.Type);
+            Assert.AreEqual("v", actualArgument.Name);
+            Assert.IsNull(actualArgument.Parameter);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+
+        [TestMethod()]
+        public void CreateTestLongOptionWithDash()
+        {
+            Argument actualArgument = Argument.Create("--long-option");
+            Assert.AreEqual(ArgumentType.longOption, actualArgument.Type);
+            Assert.AreEqual("long-option", actualArgument.Name);
+            Assert.IsNull(actualArgument.Parameter);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+
+        [TestMethod()]
+        public void CreateTestLongOptionEqual()
+        {
+            Argument actualArgument = Argument.Create("--option=3test");
+            Assert.AreEqual(ArgumentType.longOption, actualArgument.Type);
+            Assert.AreEqual("option", actualArgument.Name);
+            Assert.AreEqual("3test", actualArgument.Parameter);
+            Assert.IsNull(actualArgument.ProgramArgument);
+        }
+
+    }
+}
--- TestEasyOpt/Properties/AssemblyInfo.cs
+++ TestEasyOpt/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EasyOpt")]
+[assembly: AssemblyTitle("TestEasyOpt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EasyOpt")]
+[assembly: AssemblyProduct("TestEasyOpt")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4f4c345d-3183-4c74-8f42-cd09821867dd")]
+[assembly: Guid("78280916-88cb-49c9-8804-0620526b5f7a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestEasyOpt")]

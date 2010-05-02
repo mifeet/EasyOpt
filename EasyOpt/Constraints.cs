ccfce59519555e66e1ff2169e1ed19fb6a1f69e5
Jan Michelfeit <michelfeit.jan@gmail.com>
2010-05-02 20:35:20 +0000

    Constraints in a separate file

---
+++ EasyOpt/Constraints.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyOpt
+{
+    /**
+     * Interface of a parameter constraint.
+     * @param T Type to which the constraint can be applied
+     * @see EasyOpt.Parameter< T >.AddConstraint()
+     */
+    public interface IConstraint<T>
+    {
+        /**
+         * Checks whether the constraint holds for parameter's argument.
+         * @param parameterValue value of a parameter's argument converted to type T
+         */
+        bool IsValid(T parameterValue);
+    }
+
+    /**
+     * Parameter constraint validating lower bound for its argument.
+     */
+    public sealed class LowerBoundConstraint : IConstraint<int>
+    {
+        /**
+         * Lower bound value
+         */
+        private int limit;
+
+        /**
+         * @param limit lower bound value
+         */
+        public LowerBoundConstraint(int limit)
+        {
+            this.limit = limit;
+        }
+
+        /**
+         * Check whether parameter is grater or equal to the specified
+         * lower bound.
+         * @param parameter value of a Parameter's argument
+         */
+        public bool IsValid(int parameter)
+        {
+            return parameter >= limit;
+        }
+    }
+
+    /**
+     * Parameter constraint validating upper bound for its argument.
+     */
+    public sealed class UpperBoundConstraint : IConstraint<int>
+    {
+        /**
+         * Uppper bound value
+         */
+        private int limit;
+
+        /**
+         * @param limit upper bound value
+         */
+        public UpperBoundConstraint(int limit)
+        {
+            this.limit = limit;
+        }
+
+        /**
+         * Check whether parameter is less or equal to the specified
+         * upper bound.
+         * @param parameter value of a Parameter's argument
+         */
+        public bool IsValid(int parameter)
+        {
+            return parameter <= limit;
+        }
+    }
+
+    /**
+     * Parameter constraint validating whether its argument is within
+     * the specified list of String values.
+     */
+    public sealed class StringEnumerationConstraint : IConstraint<String>
+    {
+        /**
+         * Collection of accepted values.
+         */
+        private IEnumerable<String> acceptedValues;
+
+        /**
+         * Comparer used to compare arguments to the list of accepted values.
+         */
+        private IEqualityComparer<String> comparer;
+
+        /**
+         * @param acceptedValues Collection of accepted values.
+         * @param Comparer used to compare arguments to the list of accepted values.
+         */
+        public StringEnumerationConstraint(IEnumerable<String> acceptedValues, IEqualityComparer<String> comparer)
+        {
+            if (acceptedValues == null)
+            {
+                throw new Exception(); // TODO
+            }
+            this.acceptedValues = acceptedValues;
+            this.comparer = comparer;
+        }
+
+        /**
+         * @param acceptedValues Collection of accepted values.
+         * @param If true, ignore case; otherwise, regard case.
+         */
+        public StringEnumerationConstraint(IEnumerable<String> acceptedValues, bool ignoreCase)
+            : this(
+                acceptedValues,
+                ignoreCase ? StringComparer.InvariantCultureIgnoreCase : StringComparer.InvariantCulture
+            )
+        { }
+
+        /**
+         * Check whether parameter is within the specified range of accepted values.
+         * @param parameter value of a Parameter's argument
+         */
+        public bool IsValid(String parameter)
+        {
+            return acceptedValues.Contains(parameter, comparer);
+        }
+    }
+
+    /**
+     * Parameter constraint validating whether its argument represent
+     * an existing file.
+     */
+    public sealed class ExistingFileConstraint : IConstraint<String>
+    {
+        /**
+         * Check whether parameter represents path to an existing file.
+         * The path can be relative or absolute. Relative path information
+         * is interpreted as relative to the current working directory
+         * @param parameter value of a Parameter's argument
+         */
+        public bool IsValid(String parameter)
+        {
+            return System.IO.File.Exists(parameter);
+        }
+    }
+}

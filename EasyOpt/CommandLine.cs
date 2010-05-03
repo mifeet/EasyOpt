59ac5dc016d4a34c0f2cc9655021b6df1ebb2d7d
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2010-05-03 19:31:03 +0000

    Work in the parse() method, the first part that checks for command  line arguments is done but need more testing.
    The second part that verify that all required options are given
    in the command line is not done yet.

--- EasyOpt/CommandLine.cs
+++ EasyOpt/CommandLine.cs
@@ -11,10 +11,23 @@ namespace EasyOpt
      */ 
     public class ParseException : Exception
     {
+        /**
+         * Constructor method
+         * @arg message exception's detail message
+         */ 
         public ParseException(string message)
         : base(message)
         {
             
+        }
+        /**
+         * Constructor method
+         * @arg message exception's detail message
+         * @arg exception inner exception
+         */
+        public ParseException(string message, Exception exception)
+        : base(message, exception)
+        {
         }
     }
 
@@ -53,6 +66,7 @@ namespace EasyOpt
         //Object that stores the parse phase
         private ParsePhase phase;
 
+        private List<string> programArguments = new List<string>();
         
         /**
          * Initializes a CommandLine instance.
@@ -87,43 +101,172 @@ namespace EasyOpt
 
         public void Parse()
         {
+            IOption lastParsedOption = null;
+
             foreach (string unParsedArgument in unParsedArguments)
             {
                 if (phase.Equals(ParsePhase.option))
                 {
-                    Argument argument = Argument.Create(unParsedArgument);
-
-                    if (argument.Type.Equals(ArgumentType.division))
-                    {
-                        this.phase = ParsePhase.programArgument;
-                    }
-                    else if (
-                        argument.Type.Equals(ArgumentType.shortOption) ||
-                        argument.Type.Equals(ArgumentType.longOption)
-                    ){
-                        if (options.ContainsKey(argument.Name))
-                        {
-                            IOption option = options[argument.Name];
-                            option.SetValue(argument.Parameter);
-                            option.IsPresent=true;
-                        }
-                        else
-                        {
-                            throw new ParseException("Option: " + argument.Name + " is not defined.");
-                        }
-                    }
+                    lastParsedOption = parseOption(unParsedArgument);
                 }
                 else if (phase.Equals(ParsePhase.waitingForArgument))
                 {
-                    //Do not need to parse, just set the parameter of the option
+                    parseWaitingForArgument(lastParsedOption, unParsedArgument);
                 }
                 else if (phase.Equals(ParsePhase.programArgument))
                 {
-                    //Copy the program parameters, at the end of parsing put them in a string[]
+                    this.programArguments.Add(unParsedArgument);
                 }
             }
 
-            // throw new NotImplementedException();
+            checkParserFinalState(lastParsedOption);
+        }
+
+        /**
+         * Attempts to parse the given unParsedArgument as a parameter for the last parsedOption.
+         * In case is optional creates a new Option.
+         * @throw ParseException if it is not possible to parse the argument and the parameter is required.
+         */ 
+        private IOption parseWaitingForArgument(IOption lastParsedOption, string unParsedArgument)
+        {
+            if (lastParsedOption == null)
+            {
+                throw new ParseException("Internal error: lastParsedOption is null.");
+            }
+
+            this.phase = ParsePhase.option;
+
+            IOption option = null;
+
+            if (lastParsedOption.IsParameterRequired)
+            {
+                setOptionValue(lastParsedOption, unParsedArgument);
+            }
+            else
+            {
+                try
+                {
+                    setOptionValue(lastParsedOption, unParsedArgument);
+                }
+                catch (ParseException)
+                {
+                    option = parseOption(unParsedArgument);
+                }
+            }
+            return option;
+        }
+
+
+        /**
+         * Verify the final state of the parser.
+         * @throw ParseException when the parser phase has a non valid ending state.
+         */ 
+        private void checkParserFinalState(IOption lastParsedOption)
+        {
+            if (this.phase.Equals(ParsePhase.waitingForArgument) && lastParsedOption.IsParameterRequired)
+            {
+                //TODO discuss a data structure to retrieve synonyms to options, maybe another hash table.
+                throw new ParseException ("Missing argument for option: ");
+            }
+        }
+
+        /**
+         * Procedure that parses a command line argument.
+         * 
+         * @return Option associated with the argument.
+         */ 
+        private IOption parseOption(string unParsedArgument)
+        {
+
+            Argument argument = Argument.Create(unParsedArgument);
+            IOption option = null;
+
+            if (argument.Type.Equals(ArgumentType.division))
+            {
+                this.phase = ParsePhase.programArgument;
+            }
+            else if (argument.Type.Equals(ArgumentType.programArgument))
+            {
+                this.phase = ParsePhase.programArgument;
+                this.programArguments.Add(argument.ProgramArgument);
+            }
+            else if (argument.Type.IsOption())
+            {
+                checkOptionName(argument);
+
+                option = options[argument.Name];
+                option.IsPresent = true;
+
+                if (option.IsParameterRequired)
+                {
+                    parseRequiredParameter(option, argument);
+                }
+
+                if (argument.Parameter == null)
+                {
+                    this.phase = ParsePhase.waitingForArgument;
+                }
+                else
+                {
+                    setOptionValue(option, argument.Parameter);
+                }
+            }
+            return option;
+        }
+
+        /**
+         * Sets the option's value.
+         * @param option the option that is being parsed
+         * @param argument the parsed argument received from the command line.
+         * @throw ParseException when is not possible to specify the value for the object.
+         */
+        private void setOptionValue(IOption option, string value)
+        {
+            try
+            {
+                option.SetValue(value);
+            }
+            catch (ForbiddenParameterException)
+            {
+                throw new ParseException("Option: " + option.UnParsedArgument + " can't have a parameter.");
+            }
+            catch (Exception e)
+            {
+                throw new ParseException("Parameter of option: " + option.UnParsedArgument + " is invalid.", e);
+            }
+        }
+
+        /**
+         * Checks that the argument's name is valid
+         * @param argument the parsed argument received from the command line. 
+         * @throw ParseException if the name is not valid
+         */
+        private void checkOptionName(Argument argument)
+        {
+            if (!options.ContainsKey(argument.Name))
+            {
+                throw new ParseException("Option: " + argument.Name + " is not defined.");
+            }
+        }
+
+        /**
+         * Parse procedure for a parameter that is required.
+         * 
+         * @param option the option that is being parsed
+         * @param argument the parsed argument received from the command line.
+         * 
+         * @throw ParseException if the parameter is required and it is a short option.
+         */
+        private void parseRequiredParameter(IOption option, Argument argument)
+        {
+            if (
+                argument.Parameter == null && 
+                argument.Type.Equals(ArgumentType.shortOption)
+            )
+            {
+                throw new ParseException("Option: " + argument.Name + " requires a parameter.");
+
+            }
         }
 
         public String[] GetArguments() // return non-option arguments

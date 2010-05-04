07c076cf3d32a432b1ae405a50b015d0ffc24ffa
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2010-05-04 17:46:50 +0000

    Created a new abstraction IOptionContainer to store and retrieve options and their names.
    Parse() method now checks for required options not present in
    the command line.

--- EasyOpt/CommandLine.cs
+++ EasyOpt/CommandLine.cs
@@ -73,7 +73,7 @@ namespace EasyOpt
     public class CommandLine
     {
         //Object used to map option identifiers with option configurations
-        private Dictionary<String, IOption> options;
+        private IOptionContainer optionContainer;
         //Object that stores the arguments entered by the user
         private String[] unparsedArguments;
 
@@ -97,43 +97,39 @@ namespace EasyOpt
          */
         public CommandLine(String[] unparsedArguments)
         {
-            this.options = new Dictionary<string, IOption>();
+            this.optionContainer = new OptionContainer();
             this.unparsedArguments = unparsedArguments;
             this.phase = ParsePhase.Option;
         }
 
         public void AddOption(IOption option, params String[] names)
         {
-            // checkConfiguration(option); ?
-            foreach (String name in names)
-            {
-                options.Add(name, option);
-            }
+            this.optionContainer.Add(option, names);
         }
 
         public void AddOption(IOption option, char shortName)
         {
-            AddOption(option, new String[] { shortName.ToString() });
+            this.optionContainer.Add(option, new String[] { shortName.ToString() });
         }
 
         public void AddOption(IOption option, char shortName, String longName)
         {
-            AddOption(option, new String[] { shortName.ToString(), longName });
+            this.optionContainer.Add(option, new String[] { shortName.ToString(), longName });
         }
 
         public void Parse()
         {
-            IOption lastParsedOption = null;
+            Token lastParsedToken = null;
 
             foreach (string unParsedArgument in unparsedArguments)
             {
                 if (phase.Equals(ParsePhase.Option))
                 {
-                    lastParsedOption = parseOption(unParsedArgument);
+                    lastParsedToken = parseOption(unParsedArgument);
                 }
                 else if (phase.Equals(ParsePhase.WaitingForArgument))
                 {
-                    lastParsedOption = parseWaitingForArgument(lastParsedOption, unParsedArgument);
+                    lastParsedToken = parseWaitingForArgument(lastParsedToken, unParsedArgument);
                 }
                 else if (phase.Equals(ParsePhase.ProgramArgument))
                 {
@@ -141,7 +137,27 @@ namespace EasyOpt
                 }
             }
 
-            checkParserFinalState(lastParsedOption);
+            checkParserFinalState(lastParsedToken);
+
+            checkRequiredOptions();
+        }
+
+        /**
+         * Check that all required options are given in the command line arguments.
+         */ 
+        private void checkRequiredOptions()
+        {
+            List<string> uniqueNames = this.optionContainer.ListUniqueNames();
+
+            foreach (string uniqueName in uniqueNames)
+            {
+                IOption option = this.optionContainer.FindByName(uniqueName);
+                if (option.IsRequired && !option.IsPresent)
+                {
+                    throw new ParseException("Option: " + uniqueName + " is required but not specified.");
+                }
+            }
+
         }
 
         /**
@@ -149,42 +165,42 @@ namespace EasyOpt
          * In case is optional creates a new Option.
          * @throw ParseException if it is not possible to parse the argument and the parameter is required.
          */ 
-        private IOption parseWaitingForArgument(IOption lastParsedOption, string unParsedArgument)
+        private Token parseWaitingForArgument(Token lastParsedToken, string unParsedArgument)
         {
-            if (lastParsedOption == null)
+            if (lastParsedToken == null)
             {
-                throw new ParseException("Internal error: lastParsedOption is null.");
+                throw new ParseException("Internal error: lastParsedToken is null.");
             }
 
             this.phase = ParsePhase.Option;
 
-            IOption option = null;
+            Token currentToken = null;
 
-            if (lastParsedOption.Argument.Type.Equals(ArgumentType.LongOption))
+            if (lastParsedToken.Type.Equals(TokenType.LongOption))
             {
-                if (lastParsedOption.IsParameterRequired)
+                if (lastParsedToken.Option.IsParameterRequired)
                 {
-                    setOptionValue(lastParsedOption, unParsedArgument);
+                    setOptionValue(lastParsedToken, unParsedArgument);
                 }
                 else
                 {
                     try
                     {
-                        setOptionValue(lastParsedOption, unParsedArgument);
+                        setOptionValue(lastParsedToken, unParsedArgument);
                     }
                     catch (ParseException)
                     {
-                        option = parseOption(unParsedArgument);
+                        currentToken = parseOption(unParsedArgument);
                     }
                 }
             }
             else
             {
-                option = parseOption(unParsedArgument);
+                currentToken = parseOption(unParsedArgument);
             }
             
 
-            return option;
+            return currentToken;
         }
 
 
@@ -192,9 +208,9 @@ namespace EasyOpt
          * Verify the final state of the parser.
          * @throw ParseException when the parser phase has a non valid ending state.
          */ 
-        private void checkParserFinalState(IOption lastParsedOption)
+        private void checkParserFinalState(Token lastParsedToken)
         {
-            if (this.phase.Equals(ParsePhase.WaitingForArgument) && lastParsedOption.IsParameterRequired)
+            if (this.phase.Equals(ParsePhase.WaitingForArgument) && lastParsedToken.Option.IsParameterRequired)
             {
                 //TODO discuss a data structure to retrieve synonyms to options, maybe another hash table.
                 throw new ParseException ("Missing argument for option: ");
@@ -206,78 +222,72 @@ namespace EasyOpt
          * 
          * @return Option associated with the argument.
          */ 
-        private IOption parseOption(string unParsedArgument)
+        private Token parseOption(string unParsedArgument)
         {
+            Token lastParsedToken = null;
+            
+            List<Token> tokens = Token.Create(unParsedArgument, optionContainer);
 
-            Argument argument = Argument.Create(unParsedArgument);
-            IOption option = null;
-
-            if (argument.Type.Equals(ArgumentType.Division))
+            foreach (Token token in tokens)
             {
-                this.phase = ParsePhase.ProgramArgument;
-            }
-            else if (argument.Type.Equals(ArgumentType.ProgramArgument))
-            {
-                this.phase = ParsePhase.ProgramArgument;
-                this.programArguments.Add(argument.ProgramArgument);
-            }
-            else if (argument.Type.IsOption())
-            {
-                checkOptionName(argument);
+                lastParsedToken = token;
 
-                option = options[argument.Name];
-                option.IsPresent = true;
-                option.Argument = argument;
-
-                if (option.IsParameterRequired)
+                if (token.Type.Equals(TokenType.Division))
                 {
-                    parseRequiredParameter(option, argument);
+                    this.phase = ParsePhase.ProgramArgument;
+                }
+                else if (token.Type.Equals(TokenType.ProgramArgument))
+                {
+                    this.phase = ParsePhase.ProgramArgument;
+                    this.programArguments.Add(token.ProgramArgument);
+                }
+                else if (token.Type.IsOption())
+                {
+                    IOption option = optionContainer.FindByName(token.Name);
+                    option.IsPresent = true;
+                    token.Option = option;
+
+                    if (option.IsParameterRequired)
+                    {
+                        parseRequiredParameter(option, token);
+                    }
+
+                    if (token.Parameter == null)
+                    {
+                        if (token.Type.Equals(TokenType.LongOption)){
+                            this.phase = ParsePhase.WaitingForArgument;
+                        }
+                    }
+                    else
+                    {
+                        setOptionValue(token, token.Parameter);
+                    }
                 }
 
-                if (argument.Parameter == null)
-                {
-                    this.phase = ParsePhase.WaitingForArgument;
-                }
-                else
-                {
-                    setOptionValue(option, argument.Parameter);
-                }
             }
-            return option;
+
+            return lastParsedToken;
         }
 
         /**
          * Sets the option's value.
-         * @param option the option that is being parsed
+         * @param token the token that is being parsed
          * @param argument the parsed argument received from the command line.
          * @throw ParseException when is not possible to specify the value for the object.
          */
-        private void setOptionValue(IOption option, string value)
+        private void setOptionValue(Token token, string value)
         {
             try
             {
-                option.SetValue(value);
+                token.Option.SetValue(value);
             }
             catch (ForbiddenParameterException)
             {
-                throw new ParseException("Option: " + option.Argument.UnparsedText + " can't have a parameter.");
+                throw new ParseException("Option: " + token.UnparsedText + " can't have a parameter.");
             }
             catch (Exception e)
             {
-                throw new ParseException("Parameter of option: " + option.Argument.UnparsedText + " is invalid.", e);
-            }
-        }
-
-        /**
-         * Checks that the argument's name is valid
-         * @param argument the parsed argument received from the command line. 
-         * @throw ParseException if the name is not valid
-         */
-        private void checkOptionName(Argument argument)
-        {
-            if (!options.ContainsKey(argument.Name))
-            {
-                throw new ParseException("Option: " + argument.Name + " is not defined.");
+                throw new ParseException("Parameter of option: " + token.UnparsedText + " is invalid.", e);
             }
         }
 
@@ -289,11 +299,11 @@ namespace EasyOpt
          * 
          * @throw ParseException if the parameter is required and it is a short option.
          */
-        private void parseRequiredParameter(IOption option, Argument argument)
+        private void parseRequiredParameter(IOption option, Token argument)
         {
             if (
                 argument.Parameter == null && 
-                argument.Type.Equals(ArgumentType.ShortOption)
+                argument.Type.Equals(TokenType.ShortOption)
             )
             {
                 throw new ParseException("Option: " + argument.Name + " requires a parameter.");
